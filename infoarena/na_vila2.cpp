111b6a41d608594ab0a431c032aefe748e1dbda8
Roland Petrean <roland.petrean@gmail.com>
2022-08-24 19:56:04 +0300

    update

---
+++ infoarena/na_vila2.cpp
@@ -0,0 +1,37 @@
+// https://infoarena.ro/problema/vila2
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define ull unsigned long long
+#define mod 1000000007
+
+ifstream fin("vila2.in");
+ofstream fout("vila2.out");
+
+int main() {
+  int n, k;
+  fin>>n>>k;
+
+  vector<int> a(n);
+  deque<int> maxi, mini;
+  for (int i=0; i<k; ++i) {
+    fin>>a[i];
+    while (!maxi.empty() && a[i]>a[maxi.back()]) maxi.pop_back();
+    while (!mini.empty() && a[i]<a[mini.back()]) mini.pop_back();
+    maxi.push_back(i);
+    mini.push_back(i);
+  }
+
+  int maxd=a[maxi.front()]-a[mini.front()];
+  for (int i=k; i<n; ++i) {
+    fin>>a[i];
+    while (!maxi.empty() && maxi.front() < i-k) maxi.pop_front();
+    while (!maxi.empty() && a[i]>a[maxi.back()]) maxi.pop_back();
+    while (!mini.empty() && mini.front() < i-k) mini.pop_front();
+    while (!mini.empty() && a[i]<a[mini.back()]) mini.pop_back();
+    maxi.push_back(i);
+    mini.push_back(i);
+    maxd = max(maxd, a[maxi.front()]-a[mini.front()]);
+  }
+  fout<<maxd;
+}
---
+++ varena/4_disjoint.cpp
@@ -0,0 +1,50 @@
+// https://www.varena.ro/problema/disjoint
+// TLE la 50% din teste, am furat codul de jos fix de pe cp-algorithms si tot nu merge. Cred ca
+// trebuie scris in C pentru pb asta, ca altfel nu imi explic de ce nu functioneaza.
+// (Fac union find by rank cu path compression)
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define ull unsigned long long
+#define mod 1000000007
+
+ifstream fin("disjoint.in");
+ofstream fout("disjoint.out");
+
+array<int,100000> parent{}, r{};
+
+int find_set(int v) {
+  if (v == parent[v])
+    return v;
+  return parent[v] = find_set(parent[v]);
+}
+
+void union_sets(int a, int b) {
+  a = find_set(a);
+  b = find_set(b);
+  if (a != b) {
+    if (r[a] < r[b])
+      swap(a, b);
+    parent[b] = a;
+    if (r[a] == r[b])
+      r[a]++;
+  }
+}
+
+int main() {
+  int n, m;
+  fin>>n>>m;
+
+  for (int i=0; i<n; ++i) parent[i] = i;
+  for (int i=0; i<m; ++i) {
+    int cod, x, y;
+    fin>>cod>>x>>y;
+    --x; --y;
+    if (cod==1) {
+      union_sets(x, y);
+    } else if (cod==2) {
+      if (find_set(x) == find_set(y)) fout<<"DA"<<endl;
+      else fout<<"NU"<<endl;
+    }
+  }
+}
